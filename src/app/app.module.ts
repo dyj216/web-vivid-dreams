9d55dc911e963a4133141bedf13b0f868a1c7f50
Zsolt Endreffy <zsolt.endreffy@balabit.com>
2020-03-28 18:17:49 +0100

    conformize imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {MatButtonModule} from '@angular/material/button';
+import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatSidenavModule } from '@angular/material/sidenav';
-import {MatListModule} from '@angular/material/list';
+import { MatListModule } from '@angular/material/list';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -12,10 +12,10 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { GameSetupComponent } from './game-setup/game-setup.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { RulesComponent } from './rules/rules.component';
-import {MatInputModule} from "@angular/material/input";
-import {MatSelectModule} from "@angular/material/select";
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
 import { GuessComponent } from './guess/guess.component';
-import {MatGridListModule} from "@angular/material/grid-list";
+import { MatGridListModule } from '@angular/material/grid-list';
 import { RememberComponent } from './remember/remember.component';
 import { ScoreComponent } from './score/score.component';
 
