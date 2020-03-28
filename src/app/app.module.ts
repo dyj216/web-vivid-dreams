e500c6cc07e0b5ea3602498c4a36c68837b1c3ad
Zsolt Endreffy <zsolt.endreffy@balabit.com>
2020-03-28 18:19:14 +0100

    add guess component functionality

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatListModule } from '@angular/material/list';
+import { CountdownModule } from 'ngx-countdown';
 import { FormsModule } from '@angular/forms'
 
 import { AppRoutingModule } from './app-routing.module';
@@ -34,6 +35,7 @@ import { ScoreComponent } from './score/score.component';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    CountdownModule,
     FormsModule,
     MatButtonModule,
     MatToolbarModule,
--- src/app/guess/guess.component.ts
+++ src/app/guess/guess.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {GameService} from '../game.service';
+import {CountdownComponent, CountdownConfig, CountdownEvent, CountdownStatus} from 'ngx-countdown';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-guess',
@@ -6,10 +9,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./guess.component.scss']
 })
 export class GuessComponent implements OnInit {
+  countdownConfig: CountdownConfig;
+  skipButtonEnabled = false;
 
-  constructor() { }
+  @ViewChild('cd') private countdown: CountdownComponent;
+
+  constructor(
+    public gameService: GameService,
+    private router: Router,
+  ) { }
 
   ngOnInit() {
+    this.gameService.shuffleWords();
+    this.countdownConfig = {
+      leftTime: this.gameService.roundDuration,
+      format: 'mm:ss'
+    };
+  }
+
+  fabPressed(newPlace: string[]) {
+    newPlace.push(this.gameService.words.shift());
+    if (this.skipButtonEnabled) {
+      this.router.navigate(['/', 'remember']);
+    }
+  }
+
+  handleCountdownEvent(event: CountdownEvent) {
+    if (event.status === CountdownStatus.done) {
+      this.skipButtonEnabled = true;
+    }
   }
 
 }
